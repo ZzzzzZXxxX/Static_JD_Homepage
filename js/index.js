82fc2d15ad8406df094ca83d64e9f77d2bdc1093
ZzzzzZXxxX <zaqzuo@foxmail.com>
2019-12-06 15:07:54 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+window.onload = function() {
+	var leftmenu = document.getElementById("leftmenu");
+	var Lis = leftmenu.getElementsByTagName("li");
+	for (i = 0; i < Lis.length; i++) {
+		Lis[i].onmouseover = function() {
+			this.className = "lihover";
+		}
+		Lis[i].onmouseout = function() {
+			this.className = "";
+		}
+	}
+	
+	
+}
+
+
---
+++ js/test.js
@@ -0,0 +1,25 @@
+var imgList = document.getElementById("imgList");
+var Lis = imgList.getElementsByTagName("li");
+var number = document.getElementById("number");
+var n_ls = number.getElementsByTagName("li");
+var index = 0
+// 重复延时循环
+setInterval(function() {
+	console.log(index);
+	if (index >= 0 && index <= 4) {
+		Lis[index].children[0].style.display = "none";
+		n_ls[index].style.background = "";
+		Lis[index + 1].children[0].style.display = "block";
+		n_ls[index + 1].style.background = "white";
+
+	} else {
+		Lis[index].children[0].style.display = "none";
+		n_ls[index].style.background = "";
+		index = 0;
+		Lis[index].children[0].style.display = "block";
+		n_ls[index].style.background = "white";
+		return;
+	}
+	index++;
+
+}, "1500")
